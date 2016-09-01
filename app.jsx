0f82ec8d05f76fd30bb754276e884ca6125aa196
liucheng <liu_cheng@trendmicro.com>
2016-09-01 16:38:03 -0400

    Initial commit

---
+++ app.jsx
@@ -0,0 +1,52 @@
+function Application() {
+  return (
+      <div className="scoreboard">
+          <div className="header">
+              <h1>Scoreboard</h1>
+          </div>
+
+          <div className="players">
+              <div className="player">
+                  <div className="player-name">
+                      Jim Hoskins
+                  </div>
+                  <div className="player-score">
+                      <div className="counter">
+                          <button className="counter-action decrement"> - </button>
+                          <div className="counter-score"> 31 </div>
+                          <button className="counter-action increment"> + </button>
+                      </div>
+                  </div>
+              </div>
+
+              <div className="player">
+                  <div className="player-name">
+                      Willow Cheng
+                  </div>
+                  <div className="player-score">
+                      <div className="counter">
+                          <button className="counter-action decrement"> - </button>
+                          <div className="counter-score"> 12 </div>
+                          <button className="counter-action increment"> + </button>
+                      </div>
+                  </div>
+              </div>
+
+              <div className="player">
+                  <div className="player-name">
+                      David Willan
+                  </div>
+                  <div className="player-score">
+                      <div className="counter">
+                          <button className="counter-action decrement"> - </button>
+                          <div className="counter-score"> 23 </div>
+                          <button className="counter-action increment"> + </button>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById('container'));
