1dec1e679002841a0c968cdf6bf37bf4fd5f0d64
willowcheng <e.wingus@Gmail.com>
2016-09-01 16:52:03 -0400

    Proptypes and defaultProps

--- app.jsx
+++ app.jsx
@@ -49,4 +49,12 @@ function Application(props) {
   );
 }
 
-ReactDOM.render(<Application title="My Scoreboard"/>, document.getElementById('container'));
+Application.propTypes = {
+    title: React.PropTypes.string
+};
+
+Application.defaultProps = {
+    title: "Scoreboard"
+};
+
+ReactDOM.render(<Application/>, document.getElementById('container'));
