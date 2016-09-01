8c0b04c2eb2e0abdb4e37eeaf03007c18b937f98
willowcheng <e.wingus@Gmail.com>
2016-09-01 17:05:58 -0400

    Decompose the application

--- app.jsx
+++ app.jsx
@@ -1,49 +1,55 @@
+function Header(props) {
+    return (
+        <div className="header">
+            <h1>{props.title}</h1>
+        </div>
+    );
+}
+
+Header.propTypes = {
+    title: React.PropTypes.string
+};
+
+function Counter(props) {
+    return (
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <div className="counter-score"> {props.score} </div>
+            <button className="counter-action increment"> + </button>
+        </div>
+    );
+}
+
+Counter.propTypes = {
+    score: React.PropTypes.string.isRequired
+};
+
+function Player(props) {
+    return (
+        <div className="player">
+            <div className="player-name">
+                {props.name}
+            </div>
+            <div className="player-score">
+                <Counter score={props.score} />
+            </div>
+        </div>
+    );
+}
+
+Player.propTypes = {
+    name: React.PropTypes.string.isRequired,
+    score: React.PropTypes.number.isRequired
+};
+
 function Application(props) {
   return (
       <div className="scoreboard">
-          <div className="header">
-              <h1>{props.title}</h1>
-          </div>
+          <Header title={props.title} />
 
           <div className="players">
-              <div className="player">
-                  <div className="player-name">
-                      Jim Hoskins
-                  </div>
-                  <div className="player-score">
-                      <div className="counter">
-                          <button className="counter-action decrement"> - </button>
-                          <div className="counter-score"> 31 </div>
-                          <button className="counter-action increment"> + </button>
-                      </div>
-                  </div>
-              </div>
-
-              <div className="player">
-                  <div className="player-name">
-                      Willow Cheng
-                  </div>
-                  <div className="player-score">
-                      <div className="counter">
-                          <button className="counter-action decrement"> - </button>
-                          <div className="counter-score"> 12 </div>
-                          <button className="counter-action increment"> + </button>
-                      </div>
-                  </div>
-              </div>
-
-              <div className="player">
-                  <div className="player-name">
-                      David Willan
-                  </div>
-                  <div className="player-score">
-                      <div className="counter">
-                          <button className="counter-action decrement"> - </button>
-                          <div className="counter-score"> 23 </div>
-                          <button className="counter-action increment"> + </button>
-                      </div>
-                  </div>
-              </div>
+              <Player name="Jim Hoskins" score={31} />
+              <Player name="Andrew Chalkley" score={33} />
           </div>
       </div>
   );
