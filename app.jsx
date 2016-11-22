08675b00484aa08c99441086d89afe0476e00e21
willowcheng <e.wingus@gmail.com>
2016-11-22 11:21:59 -0500

    Make stopwatch tick

--- app.jsx
+++ app.jsx
@@ -21,24 +21,50 @@ var nextId = 4;
 var Stopwatch = React.createClass({
     getInitialState: function () {
         return {
-            running: false
+            running: false,
+            elapsedTime: 0,
+            previousTime: 0
         }
     },
+    componentDidMount: function () {
+        this.interval = setInterval(this.onTick, 100);
+    },
+    componentWillUnmount: function () {
+        clearInterval(this.interval);
+    },
+    onTick: function () {
+        if (this.state.running) {
+            var now = Date.now();
+            this.setState({
+                previousTime: now,
+                elapsedTime: this.state.elapsedTime + (now - this.state.previousTime)
+            });
+        }
+        console.log('onTick');
+    },
     onStart: function () {
-        this.setState({ running: true});
+        this.setState({
+            running: true,
+            previousTime: Date.now()
+        });
     },
     onStop: function () {
-        this.setState({ running: false});
+        this.setState({running: false});
     },
     onReset: function () {
-
+        this.setState({
+            elapsedTime: 0,
+            previousTime: Date.now()
+        })
     },
     render: function () {
+        var seconds = Math.floor(this.state.elapsedTime / 1000);
         return (
             <div className="stopwatch">
                 <h2>Stopwatch</h2>
-                <div className="stopwatch-time">0</div>
-                { this.state.running ? <button onClick={this.onStop}>Stop</button> : <button onClick={this.onStart}>Start</button> }
+                <div className="stopwatch-time">{seconds}</div>
+                { this.state.running ? <button onClick={this.onStop}>Stop</button> :
+                    <button onClick={this.onStart}>Start</button> }
                 <button onClick={this.onReset}>Reset</button>
             </div>
         )
