cf614cdd37f462f70091eddb9607bbb27dff9444
willowcheng <e.wingus@gmail.com>
2016-11-22 11:01:52 -0500

    Design the stop watch

--- app.jsx
+++ app.jsx
@@ -18,6 +18,19 @@ var PLAYERS = [
 
 var nextId = 4;
 
+var Stopwatch = React.createClass({
+    render: function () {
+        return (
+            <div className="stopwatch">
+                <h2>Stopwatch</h2>
+                <div className="stopwatch-time">0</div>
+                <button>Start</button>
+                <button>Reset</button>
+            </div>
+        )
+    }
+});
+
 var AddPlayerForm = React.createClass({
     propTypes: {
         onAdd: React.PropTypes.func.isRequired
@@ -80,6 +93,7 @@ function Header(props) {
         <div className="header">
             <Stats players={props.players}/>
             <h1>{props.title}</h1>
+            <Stopwatch/>
         </div>
     );
 }
